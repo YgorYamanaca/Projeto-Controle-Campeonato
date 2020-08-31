3122cbb0842f9923cf1463541375e668614f207b
DaniloFerraz <daniloferraz97@gmail.com>
2020-08-30 22:21:14 -0300

    img na home

--- display/src/page/Home/index.js
+++ display/src/page/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Link, Switch, Route} from 'react-router-dom'
 import Register from '../../page/Register'
 import AppStylizedButton from '../../components/AppStylizedButton/'
-import { HomePageSty, HomeTopSty, HomeContentSty, TopTextStyle } from './styles.js'
+import {LogoEditor, Descricao, HomeContainer } from './styles.js'
+import logo from '../../medias/logo.png'
 
 export default function Home() {
     return (
-        <div>
-            
-        </div>
+        <HomeContainer>
+            aaaaaaaaa
+            <LogoEditor src={logo} alt="logo"/> 
+        </HomeContainer>
     )
 }
---
+++ display/src/page/Home/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const LogoEditor = styled.img`
+    width:250px;
+    height:250px;
+  
+
+`;
+
+export const HomeContainer = styled.div`
+    display:flex;
+    justify-content: center;
+    align-itens: center;
+`;
+
